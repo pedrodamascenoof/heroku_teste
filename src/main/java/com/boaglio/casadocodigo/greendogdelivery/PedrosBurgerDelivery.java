5726a94fcf4dbe4e618ddc05c0e6307bab27745b
pedrodamascenoof <pedrodamasceno88@gmail.com>
2022-09-30 14:06:45 -0300

    teste de alteracao

---
+++ src/main/java/com/boaglio/casadocodigo/greendogdelivery/PedrosBurgerDelivery.java
@@ -0,0 +1,14 @@
+package com.boaglio.casadocodigo.greendogdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.boaglio")
+public class PedrosBurgerDelivery {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedrosBurgerDelivery.class, args);
+	}
+}
